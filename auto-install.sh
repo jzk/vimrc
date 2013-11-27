34cd8a63d7105d299d07f0c829bd3120a208af4d
Zhenkai Jiang <zk.jiang@gmail.com>
2013-11-27 23:02:28 +0800

    init commit with nearly empty vimrc and install scripts

---
+++ auto-install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+VIMHOME=~/.vim
+
+warn() {
+    echo "$1" >&2
+}
+
+die() {
+    warn "$1"
+    exit 1
+}
+
+[[ -e "$VIMHOME" ]] && mv $VIMHOME $VIMHOME.bk
+
+git clone git://github.com/vgod/vimrc.git "$VIMHOME"
+cd "$VIMHOME"
+git submodule update --init
+
+./install-vimrc.sh
+
+echo "Jzk's vimrc is installed."
---
+++ install-vimrc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VIMHOME="`pwd`"
+cd ~
+[[ -e .vimrc ]] && mv .vimrc .vimrc_origin
+[[ -e .gvimrc ]] && mv .gvimrc .gvimrc_origin
+ln -sf $VIMHOME/vimrc .vimrc
+ln -sf $VIMHOME/gvimrc .gvimrc
